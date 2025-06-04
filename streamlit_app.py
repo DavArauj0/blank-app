ec0282dd799ee428e269a4dd1cbcfeb19cd8a7b1
DavArauj0 <161933763+DavArauj0@users.noreply.github.com>
2025-06-04 09:50:58 +0000

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,35 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+"""
+1. Hola mundo interactivo
+"""
+st.title("🎈 Hello World!")
+nombre = st.text_input("¿Cómo te llamas?")
+
+if 'msg' not in st.session_state:
+    st.write("")
+
+if nombre:
+    st.session_state.msg = f"Hello, {nombre}!"
+    st.write(st.session_state.msg)
+
+
+"""
+2. Contador de clicks
+"""
+st.title("🎈 Clicks counter")
+if 'counter' not in st.session_state:
+    st.session_state.counter = 0
+
+if st.button("Increase counter"):
+    st.session_state.counter += 1
+st.write(st.session_state.counter)
+
+
+"""
+3. Selector de color
+"""
+st.title("🎈 Color selector")
+color = st.color_picker("Choose a colour", "#ff00ff")
+st.write(f"Has seleccionado el color {color}")
+
